6982e765cea59c14b240fefe4cf8575840299883
Tao Bian <tideabyan@gmail.com>
2017-02-28 17:34:04 -0500

    Feb 28

--- luaADP/test.lua
+++ luaADP/test.lua
@@ -1,8 +1,27 @@
 controller= require('ControllerVI')
 ffi = require('ffi')
-local control= ControllerVI()
---io.write(string.format("'%s' is years old\n", control:learn(ffi.new("double[3]", {1,2,3}),ffi.new("double[1]", {1}), 1,2)))
+matrix = require('matrix')
+control= ControllerVI()
 c = control:learn(ffi.new("double[3]", {1,2,3}),ffi.new("double[1]", {1}), 3, 1, 1,2)
+
+
+A = matrix{{-1,2,1},{0.1,-1,2},{0,0.1,1}}
+B = matrix{{0},{0},{1}}
+x = matrix{{2},{0},{0}}
+K = matrix{{2,0,0}}
+
+matrix.print(B*2)
+dt = 0.0001
+t = 0
+
+for i = 1,10 do
+	u = -matrix.mul(K,x)
+	dx = (matrix.mul(A,x) + matrix.mul(B,u)) * dt
+	x = x + dx
+	K = control:learn(ffi.new("double[3]", x),ffi.new("double[1]", u), 3, 1, dt,t)
+	i = i+1
+	t = t + dt
+end
 --c = learn(control,ffi.new("double[3]", {1,2,3}),ffi.new("double[1]", {1}), 3, 1, 1,2)
 --print(c[1])
 
